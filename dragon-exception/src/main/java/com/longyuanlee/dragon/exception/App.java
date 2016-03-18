a8cd338c02ad561ab775b95c729dd5e7c9034953
longyuanlee <longyuanlee@sina.com>
2016-03-18 15:17:37 +0800

    初始化项目
    Change-Id: Id4265d58f0fd147caf24fe6f16bbb1c0b156a50f
    Signed-off-by: longyuanlee <longyuanlee@sina.com>

---
+++ dragon-exception/src/main/java/com/longyuanlee/dragon/exception/App.java
@@ -0,0 +1,13 @@
+package com.longyuanlee.dragon.exception;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
